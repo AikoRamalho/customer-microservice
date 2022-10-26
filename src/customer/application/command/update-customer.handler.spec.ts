48ca54454f192d10a14652f08d3a2d8f1e6f692c
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 06:41:18 -0300

    test: ensure update-customer.handler throws when newId already in use

---
+++ src/customer/application/command/update-customer.handler.spec.ts
@@ -0,0 +1,47 @@
+import {
+  ConflictException,
+  HttpStatus,
+  ModuleMetadata,
+  Provider,
+} from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import { Test } from '@nestjs/testing';
+import { CustomerRepository } from 'src/customer/domain/customer.interface';
+import { InjectionToken } from '../injection.token';
+import { UpdateCustomerCommand } from './update-customer.command';
+import { UpdateCustomerHandler } from './update-customer.handler';
+
+describe('UpdatePasswordHandler', () => {
+  let handler: UpdateCustomerHandler;
+  let repository: CustomerRepository;
+
+  beforeEach(async () => {
+    const repositoryProvider: Provider = {
+      provide: InjectionToken.CUSTOMER_REPOSITORY,
+      useValue: {},
+    };
+    const providers: Provider[] = [UpdateCustomerHandler, repositoryProvider];
+    const moduleMetadata: ModuleMetadata = { providers };
+    const testModule = await Test.createTestingModule(moduleMetadata).compile();
+
+    handler = testModule.get(UpdateCustomerHandler);
+    repository = testModule.get(InjectionToken.CUSTOMER_REPOSITORY);
+  });
+
+  describe('execute', () => {
+    it('should return 409 when new id already in use', async () => {
+      const command = new UpdateCustomerCommand(
+        '6b157ad8-989f-4422-8701-6a0813f766a2',
+        '6b157ad8-989f-4422-8701-6a0813f766a3',
+        'new name',
+        123456789,
+      );
+
+      repository.checkIfNewIdAlreadyInUse = jest.fn().mockReturnValueOnce(true);
+
+      expect(handler.execute(command)).rejects.toThrow(
+        new HttpException('conflito de ID', HttpStatus.CONFLICT),
+      );
+    });
+  });
+});
