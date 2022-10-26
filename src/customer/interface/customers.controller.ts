0010dbf91a993377a9dc8c5f871b857c90c7399c
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 01:21:48 -0300

    refactor: delete findcustomerbyid param dto and add validation on param inside controller

--- src/customer/interface/customers.controller.ts
+++ src/customer/interface/customers.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { isUUID } from 'class-validator';
 import { CreateCustomerCommand } from 'src/customer/application/command/create-customer.command';
 import { CreateCustomerDTO } from './dto/create-customer.dto';
-import { FindCustomerByIdParamDto } from './dto/find-customer-by-id.param.dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(readonly commandBus: CommandBus) {}
   @Get(':id')
-  getCustomerById(@Param() param: FindCustomerByIdParamDto): string {
+  getCustomerById(@Param() param): string {
+    // na documentacao do desafio eh pedido para nao retornar id invalido
+    // mas sabemos que se o id n for uuid o cliente ja n existe
+    if (!isUUID(param.id)) {
+      throw new HttpException('cliente inexistente', 404);
+    }
     return param.id;
   }
 
--- src/customer/interface/dto/find-customer-by-id.param.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class FindCustomerByIdParamDto {
-  @IsUUID()
-  id: string;
-}
