b63c3fc0a1421fd74a833459726c3a6f2cc92e47
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-25 23:27:07 -0300

    feat: create handler for when customer command is dispatched

--- src/customer/application/command/create-customer.handler.ts
+++ src/customer/application/command/create-customer.handler.ts
@@ -1,11 +1,27 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { Inject } from '@nestjs/common';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { randomUUID } from 'crypto';
+import { CustomerAggregateRoot } from 'src/customer/domain/customer';
 import { CreateCustomerCommand } from './create-customer.command';
 
 @CommandHandler(CreateCustomerCommand)
 export class CreateCustomerHandler
   implements ICommandHandler<CreateCustomerCommand>
 {
+  constructor(
+    @Inject(EventPublisher) private readonly eventPublisher: EventPublisher,
+  ) {}
   async execute(command: CreateCustomerCommand): Promise<string> {
+    const customerAggregate = new CustomerAggregateRoot(
+      randomUUID(),
+      command.name,
+      command.document,
+    );
+    const customer = this.eventPublisher.mergeObjectContext(
+      customerAggregate.createCustomer(),
+    );
+    console.log('customer ', customer);
+    customer.commit();
     return `Customer ${command.name} created`;
   }
 }
--- src/customer/customers.module.ts
+++ src/customer/customers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateCustomerHandler } from 'src/customer/application/command/create-customer.handler';
+import { CustomerCreatedHandler } from './application/event/customer-created.handler';
 import { CustomersController } from './interface/customers.controller';
 
-const application = [CreateCustomerHandler];
+const application = [CreateCustomerHandler, CustomerCreatedHandler];
 
 @Module({
   imports: [CqrsModule],
