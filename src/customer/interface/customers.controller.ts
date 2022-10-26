8b9b061fc433d537d0dd81fa9427161a4793599f
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 02:44:25 -0300

    feat: add put route

--- src/customer/interface/customers.controller.ts
+++ src/customer/interface/customers.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpException,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { isUUID } from 'class-validator';
@@ -12,10 +13,13 @@ import { CreateCustomerCommand } from 'src/customer/application/command/create-c
 import { FindCustomerByIdQuery } from '../application/query/find-customer-by-id.query';
 import { FindCustomerByIdResponseDTO } from './dto/find-customer-by-id.response.dto';
 import { CreateCustomerDTO } from './dto/create-customer.dto';
+import { UpdateCustomerParam } from './dto/update-customer.param.dto';
+import { UpdateCustomerBodyDTO } from './dto/update-customer.body.dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(readonly queryBus: QueryBus, readonly commandBus: CommandBus) {}
+
   @Get(':id')
   async getCustomerById(@Param() param): Promise<FindCustomerByIdResponseDTO> {
     // na documentacao do desafio eh pedido para nao retornar id invalido
@@ -31,5 +35,13 @@ export class CustomersController {
   async createCustomer(@Body() body: CreateCustomerDTO): Promise<string> {
     const command = new CreateCustomerCommand(body.name, body.document);
     return await this.commandBus.execute(command);
+  }
+
+  @Put(':id')
+  updateCustomer(
+    @Param() param: UpdateCustomerParam,
+    @Body() body: UpdateCustomerBodyDTO,
+  ): string {
+    return `This action updates a #${param.id} customer`;
   }
 }
