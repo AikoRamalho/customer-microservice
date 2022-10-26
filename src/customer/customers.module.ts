8627e94548b29558e13b02009bef303a7fb9e2ca
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 00:59:37 -0300

    refactor: change customer.factory ecossystem to instantiate customerrepo

--- src/customer/customers.module.ts
+++ src/customer/customers.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateCustomerHandler } from 'src/customer/application/command/create-customer.handler';
 import { CustomerCreatedHandler } from './application/event/customer-created.handler';
+import { InjectionToken } from './application/injection.token';
 import { CustomerFactory } from './domain/customer.factory';
+import { CustomerRepositoryRedis } from './infra/repository/customer.repository';
 import { CustomersController } from './interface/customers.controller';
+
+const infra: Provider[] = [
+  {
+    provide: InjectionToken.CUSTOMER_REPOSITORY,
+    useClass: CustomerRepositoryRedis,
+  },
+];
 
 const application = [CreateCustomerHandler, CustomerCreatedHandler];
 
@@ -12,6 +21,6 @@ const domain = [CustomerFactory];
 @Module({
   imports: [CqrsModule],
   controllers: [CustomersController],
-  providers: [...application, ...domain],
+  providers: [...infra, ...application, ...domain],
 })
 export class CustomersModule {}
