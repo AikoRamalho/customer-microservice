f7c1cd44d854fff8629f1bb10947e92214247547
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 00:18:11 -0300

    refactor: remove unused eventpublisher from customer.factory

--- src/customer/domain/customer.factory.ts
+++ src/customer/domain/customer.factory.ts
@@ -1,13 +1,7 @@
-import { Inject } from '@nestjs/common';
-import { EventPublisher } from '@nestjs/cqrs';
 import { randomUUID } from 'crypto';
 import { CustomerAggregateRoot } from './customer.aggregate';
 
 export class CustomerFactory {
-  constructor(
-    @Inject(EventPublisher) private readonly eventPublisher: EventPublisher,
-  ) {}
-
   create(name: string, document: number): CustomerAggregateRoot {
     const id = randomUUID(); //criar como propriedade da classe automaticamente?
     const customerAggregate = new CustomerAggregateRoot(id, name, document);
