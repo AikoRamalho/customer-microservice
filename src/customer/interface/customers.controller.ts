17543f07116134c339531d462fc9de694c56b140
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 01:09:09 -0300

    refactor: add get method on customers controller

--- src/customer/interface/customers.controller.ts
+++ src/customer/interface/customers.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateCustomerCommand } from 'src/customer/application/command/create-customer.command';
 import { CreateCustomerDTO } from './dto/create-customer.dto';
+import { FindCustomerByIdParamDto } from './dto/find-customer-by-id.param.dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(readonly commandBus: CommandBus) {}
+  @Get(':id')
+  getCustomerById(@Param() param: FindCustomerByIdParamDto): string {
+    return param.id;
+  }
+
   @Post()
   async createCustomer(@Body() body: CreateCustomerDTO): Promise<string> {
     const command = new CreateCustomerCommand(body.name, body.document);
