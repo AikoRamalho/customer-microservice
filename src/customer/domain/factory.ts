5342bb0384b49acaf32da5f93733f6430a940cd8
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-10-26 05:55:48 -0300

    refactor: remove unused constructor from factory

--- src/customer/domain/factory.ts
+++ src/customer/domain/factory.ts
@@ -1,13 +1,6 @@
-import { Inject } from '@nestjs/common';
-import { InjectionToken } from 'src/customer/application/injection.token';
 import { CustomerAggregateRoot } from 'src/customer/domain/customer.aggregate';
-import { CustomerRepository } from 'src/customer/domain/customer.interface';
 
 export class CustomerFactory {
-  constructor(
-    @Inject(InjectionToken.CUSTOMER_REPOSITORY)
-    private readonly customerRepository: CustomerRepository,
-  ) {}
   create(id: string, name: string, document: number): CustomerAggregateRoot {
     //criar como propriedade da classe automaticamente?
     const customerAggregate = new CustomerAggregateRoot(id, name, document);
